17f8676278286a4a3dd7ea2e2b4eb55a98c453b2
Jasson Caceres <caceres@MBP-de-Jasson.lan>
2023-07-07 20:41:13 -0400

    first commit

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,47 @@
+import Image from 'next/image'
+import Link from 'next/link'
+import { NavLinks } from '../../constants'
+import AuthProvider from './AuthProvider'
+
+type Props = {}
+
+function Navbar({ }: Props) {
+    const session = {}
+
+    return (
+        <nav className=' flexBetween navbar'>
+            <div className=' flex-1 flexStart gap-10'>
+                <Link href={'/'}>
+                    <Image src={'/logo.svg'}
+                        width={115}
+                        height={43}
+                        alt='Flexibble' />
+                </Link>
+
+                <ul className=' xl:flex hidden text-small gap-7' >
+                    {NavLinks.map((link) => <Link href={link.href} key={link.key} > {link.text} </Link>)}
+                </ul>
+
+                <div className='flexCenter gap-4'>
+                    {session ?
+                        (
+                            <>
+                                UserPhoto
+                                <Link href='/create-project'>
+                                    Share Work
+                                </Link>
+                            </>
+                        )
+                        :
+                        (
+                            <>
+                                <AuthProvider/>
+                            </>
+                        )}
+                </div>
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar
